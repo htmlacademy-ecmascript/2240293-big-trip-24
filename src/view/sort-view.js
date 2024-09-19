d79dbc3083ef20a2d07e7b35e18c2e39e72be3ac
Danilina Mariya <mariya199828@gmail.com>
2024-09-19 13:27:23 +0700

    Реализует сортировку точек маршрута

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -5,7 +5,7 @@ import { capitalizeFirstLetter } from '../utils/common.js';
 function createSortItemTemplate(item) {
   return `<div class="trip-sort__item  trip-sort__item--${item}">
             <input id="sort-${item}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${item}" ${item === 'day' ? 'checked' : false} ${item === 'event' || item === 'offer' ? 'disabled' : false}>
-            <label class="trip-sort__btn" for="sort-${item}">${capitalizeFirstLetter(item)}</label>
+            <label class="trip-sort__btn" for="sort-${item}" data-sort-type="${item}">${capitalizeFirstLetter(item)}</label>
           </div>`;
 }
 
@@ -18,7 +18,29 @@ function createSortTemplate() {
 }
 
 export default class SortView extends AbstractView{
+  #handleSortTypeChange = null;
+  constructor({onSortTypeChange}) {
+    super();
+    this. #handleSortTypeChange = onSortTypeChange;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  }
+
   get template() {
     return createSortTemplate();
   }
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'LABEL') {
+      return;
+    }
+    //перестал ывизуально переключаться input, не нашла почему, пока сделала так
+    const inputs = this.element.querySelectorAll('input');
+    const inputChecked = this.element.querySelector(`.trip-sort__item--${evt.target.dataset.sortType}`).querySelector('input');
+    if (!inputChecked.hasAttribute('disabled')) {
+      inputs.forEach((el) => el.removeAttribute('checked'));
+      inputChecked.setAttribute('checked', '');
+    }
+    evt.preventDefault();
+    this.#handleSortTypeChange(evt.target.dataset.sortType);
+  };
 }
